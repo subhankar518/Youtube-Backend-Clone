f399547bb1f3e41ffc1cf371b52b1b6b8212ec6e
Subhankar Manna <subhankarmanna518@gmail.com>
2025-06-07 14:58:50 +0530

    user route configured

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import e from "cors";
 
 const app= express();
 
@@ -14,5 +13,19 @@ app.use(express.json({limit:"16kb"}));
 app.use(express.urlencoded({extended: true, limit: "16kb"}));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+// routes import
+import userRouter from "./routes/user.routes.js" // here we have taken differnt name, we can do that only in export default senario
+
+// Normal URL
+// Example URL: http://localhost:8000/users/register
+
+// But the good practice will be versioning of api for feture change
+// Example URL: http://localhost:8000/api/v1/users/register
+
+
+// routes declaration
+app.use("/api/v1/users",userRouter);
+
 
 export { app };
